cef3f6192821f6c850ea46fc4b75601940f20181
Tâm Trần <tamtptgcd191260@fpt.edu.vn>
2022-12-24 16:06:12 +0700

    .

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace FPTBook
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DeployConnection")));
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
